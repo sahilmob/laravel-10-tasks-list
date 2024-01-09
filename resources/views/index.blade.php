689c4cb3babb415d0842c5adacd7e3f1af6e1d1a
Sahil H. Moubaideen <sahil.mubaideen@network.global>
2024-01-09 13:07:57 +0300

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+
+@section('title', 'Tasks list')
+
+@section('content')
+    @forelse ($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">
+                {{ $task->title }}
+            </a>
+        </div>
+    @empty
+        <div>There are no tasks</div>
+    @endforelse
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Title -->
+    <title>Laravel 10 - Tasks list</title>
+
+    <!-- Styles -->
+    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+</head>
+
+<body>
+    <div id="app">
+        {{-- @include('partials.navbar') --}}
+
+        <h1>@yield('title')</h1>
+        <div>
+            @yield('content')
+        </div>
+
+    </div>
+
+    <!-- Scripts -->
+    {{-- <script src="{{ asset('js/app.js') }}"></script> --}}
+</body>
