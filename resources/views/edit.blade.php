9099ecbd7f4ef9db27caed13fab19f07e327a60e
Sahil H. Moubaideen <sahil.mubaideen@network.global>
2024-01-10 08:37:51 +0300

    reusing templates

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -12,33 +12,7 @@
 @endsection
 
 @section('content')
-    <form method="POST" action="{{ route('tasks.update', ['task' => $task->id]) }}">
-        @csrf
-        @method('PUT')
-        <div>
-            <label for="title">Title</label>
-            <input type="text" name="title" id="title" value="{{ $task->title }}">
-            @error('title')
-                <p class="error-message">{{ $message }}</p>
-            @endError
-        </div>
-        <div>
-            <label for="description">Description</label>
-            <textarea name="description" id="description" rows="5">{{ $task->description }}</textarea>
-            @error('description')
-                <p class="error-message">{{ $message }}</p>
-            @endError
-        </div>
-
-        <div>
-            <label for="long_description">Long Description</label>
-            <textarea name="long_description" id="long_description" rows="5">{{ $task->long_description }}</textarea>
-            @error('long_description')
-                <p class="error-message">{{ $message }}</p>
-            @endError
-        </div>
-        <div>
-            <button type="submit">Submit</button>
-        </div>
-    </form>
+    @include('form', [
+        'task' => $task,
+    ])
 @endsection
